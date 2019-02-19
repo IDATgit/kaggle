66f20e496ebac171b42b47606978c5da151d261f
israel rosset <israelrosset@israels-MacBook-Air.local>
2019-02-19 14:30:37 +0200

    Wrote an objective funtion for the gamma function without the rounding idea(sigmoid as a continuous rounding). The result is now 0.367 *only with the base features*

--- code/models.py
+++ code/models.py
@@ -45,9 +45,40 @@ def ranked_classes_obj(preds, train_data):
     # print("hessian", hessian)
     return grad, hessian
 
+def last_hope_obj(preds, train_data):
+    y_hat = preds
+    y = train_data.get_label()
+    N = len(y)
+
+    vals, occ = np.unique(y, return_counts=True)
+    h = dict(zip(vals, occ))
+    h = np.array([h[0], h[1], h[2], h[3], h[4]])
+
+    F = np.sum(np.square(y_hat - y))
+    G = np.sum(np.square(y_hat - 0) * h[0] +
+               np.square(y_hat - 1) * h[1] +
+               np.square(y_hat - 2) * h[2] +
+               np.square(y_hat - 3) * h[3] +
+               np.square(y_hat - 4) * h[4])
+
+    c = h[1] + 2 * h[2] + 3 * h[3] + 4 * h[4]
+
+    G_der = N * y_hat - c
+    nom = (y_hat - y) * G - F * G_der
+    grad = 2 * N * nom / (np.square(G))
+
+    hess = (G + (y_hat - y) * G_der - 2 * (y_hat - y) * G_der - N * F) * G - 2 * G_der * nom
+    hess = 2 * N * hess / np.power(G, 3)
+
+    grad, hess = np.sign(grad) * np.power(np.abs(grad), 0.1), np.array([1] * len(grad))
+    # print("y", y_hat)
+    # print("grad", grad, "\nhess", hess)
+    # print("hess sum =", np.sum(hess))
+    # print("grad sum =", np.sum(grad))
+    return grad, hess
 
 def kappa_loss(preds, train_data):
-    return "kappa", sk.metrics.cohen_kappa_score(train_data.get_label(), quantizer(preds), weights="quadratic")
+    return "kappa", sk.metrics.cohen_kappa_score(train_data.get_label(), quantizer(preds), weights="quadratic")#, True
 
 def l2_rounded_loss(preds, train_data):
     return "l2-rounded", sk.metrics.mean_squared_error(preds, train_data.get_label())
@@ -55,12 +86,43 @@ def l2_rounded_loss(preds, train_data):
 def xg_regress(X, Y):
     data = xg.DMatrix(data=X, label=Y)
     params = {
-        "eta" : 0.05,
-        "max_depth" : 6,
-        "min_child_weight" : 1,
-        "subsample" : 0.8
+        "eta" : 0.1,
+        "max_depth": 7,
+        "min_child_weight": 1,
+        # "subsample": 0.8,
+        "verbosity": 0,
+        "lambda": 1,
+        "gamma": 1
     }
     return xg.cv(params, data,
-          num_boost_round=100000,
-          nfold=3,
-          obj=ranked_classes_obj, feval=kappa_loss, maximize=True)
+                 num_boost_round=500,
+                 nfold=3,
+                 obj=last_hope_obj,
+                 feval=kappa_loss,
+                 maximize=True)
+
+
+CAT_FEATS = ['Type', 'Breed1', 'Breed2', 'Gender', 'Color1', 'Color2',
+             'Color3', 'MaturitySize', 'Vaccinated', 'Dewormed',
+             'Sterilized', 'Health', 'Quantity', 'State']
+# def xg_regress(X, Y, cat_feats=CAT_FEATS):
+#     """
+#     This function learns lgbm using a rgressor. Should go through a rounder.
+#     :return: The cv of the algorithm
+#     """
+#     data = lgbm.Dataset(X, Y, categorical_feature=cat_feats)
+#     params = {
+#         "num_leaves" : 128,
+#         "bagging_fraction" : 0.8,
+#         "bagging_freq" : 2,
+#         "feature_fraction" : 0.9,
+#         "early_stopping_round" : 10,
+#         "learning_rate" : 0.1,
+#         # "metric" : "l2",
+#         "num_iterations" : 500
+#     }
+#
+#     return lgbm.cv(params, data, categorical_feature=cat_feats,
+#                    #metrics="l2",
+#                    feval=kappa_loss, fobj=last_hope_obj,
+#                    nfold=2)
